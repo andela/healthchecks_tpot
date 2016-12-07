88a17082fc764dde011c43a0942b78eb212a6774
Ryan Marvin <marvinryan@ymail.com>
2016-12-07 15:44:02 +0300

    Pulled master that has heroku and travis integrations

---
+++ hc/settings copy.py
@@ -0,0 +1,9 @@
+DATABASES = {
+    'default': {
+        'ENGINE':   'django.db.backends.postgresql',
+        'NAME':     'hc',
+        'USER':     'ryanmarvin',
+        'PASSWORD': '',
+        'TEST': {'CHARSET': 'UTF8'}
+    }
+}
